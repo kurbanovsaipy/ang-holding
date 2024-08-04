aba00e47d5bcf60d779392e6cfd583eb80b2669d
Saypi <kurbanovsajp@gmail.com>
2024-08-04 11:25:27 +0300

    loading and not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,14 @@
-const NotFound = () => {
+import Link from "next/link";
+
+
+export default function NotFound() {
     return (
-        <div className="not-found">
-            404
+        <div className="not_found_wrapper">
+            <h2 className="not-found">
+                404.
+            </h2>
+            <p>Страница не найдена</p>
+            <Link href={'/'} className="main_button">На главную</Link>
         </div>
     );
-};
-
-export default NotFound;
+}
