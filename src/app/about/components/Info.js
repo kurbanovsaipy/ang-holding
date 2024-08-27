d450c81c1d3dd64731418db54d43ffb630098aa5
Saypi <kurbanovsajp@gmail.com>
2024-08-28 01:49:59 +0300

    upd

--- src/app/about/components/Info.js
+++ src/app/about/components/Info.js
@@ -1,9 +1,13 @@
+import Api from '@/utils/Api';
 import './../info.scss'
 
 import Consultation from "./Consultation";
 import Contacts from "./Contacts";
 
-export default function Info({info}) {
+export default async function Info() {
+
+    const info = await Api.getWithoutCache('contact/info')
+
     return (
         <div className="company_info" id="info">
             <Contacts info={info}/>
